2c074b6db0e0c5ffa4814fa7087212630b06f58c
Angga Dwi Sunarto <83453652+AnggaDS921@users.noreply.github.com>
2022-09-09 21:29:40 +0700

    mencoba perkalian matrix dengan numpy

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import numpy as np
 
 # menampilkan angka dari 0 sampai 10
+A = np.uniform(-.5, .5, size=(4,5))
+B = np.uniform(-.5, .5, size=(4,5))
+
+
+print(np.dot(A, B.T))
 print(np.arange(10, 100))
